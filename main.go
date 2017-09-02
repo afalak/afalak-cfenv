a29475a2a2c4c1bf6be420a36c04caddf9bbe9f1
Falak Ahmed <afalak@gmail.com>
2017-09-02 23:15:00 +0530

    added

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"reflect"
+
+	"github.com/cloudfoundry-community/go-cfenv"
+)
+
+// IndexHandler returns a simple message
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	appEnv, _ := cfenv.Current()
+	fmt.Fprintf(w, "Congratulations!\n\n")
+	fmt.Fprintf(w, "AppID:"+appEnv.AppID+"\n")
+	fmt.Fprintf(w, "AppName:"+appEnv.Name+"\n")
+	fmt.Fprintf(w, "Home:"+appEnv.Home+"\n")
+	fmt.Fprintf(w, "MemoryLimit:"+appEnv.MemoryLimit+"\n")
+	fmt.Fprintf(w, "SpaceName:"+appEnv.SpaceName+"\n\n")
+
+	if rmqhost, ok := appEnv.Services["rabbitmq-36"][0].Credentials["hostname"].(string); ok {
+		fmt.Fprintf(w, "rmqhost="+rmqhost+"\n")
+	}
+	if rmquser, ok := appEnv.Services["rabbitmq-36"][0].Credentials["username"].(string); ok {
+		fmt.Fprintf(w, "rmquser="+rmquser+"\n")
+	}
+	if rmqpassword, ok := appEnv.Services["rabbitmq-36"][0].Credentials["password"].(string); ok {
+		fmt.Fprintf(w, "rmqpassword="+rmqpassword+"\n")
+	}
+	if rmquri, ok := appEnv.Services["rabbitmq-36"][0].Credentials["uri"].(string); ok {
+		fmt.Fprintf(w, "rmquri="+rmquri+"\n\n")
+	}
+
+	if redishost, ok := appEnv.Services["redis-20"][0].Credentials["host"].(string); ok {
+		fmt.Fprintf(w, "redishost="+redishost+"\n")
+	}
+	if redispassword, ok := appEnv.Services["redis-20"][0].Credentials["password"].(string); ok {
+		fmt.Fprintf(w, "redispassword="+redispassword+"\n")
+	}
+	if redisport, ok := appEnv.Services["redis-20"][0].Credentials["port"].(int64); ok {
+		fmt.Fprintf(w, "redisport=", redisport, "\n\n")
+	} else {
+		var r = reflect.TypeOf(appEnv.Services["redis-20"][0].Credentials["port"])
+		fmt.Fprintf(w, "redisport=", r, "\n\n")
+	}
+
+	/**
+	for key, value := range appEnv.Services {
+		fmt.Fprintf(w, "<"+key+">", value)
+		fmt.Fprintf(w, "\n\n")
+	}**/
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+
+	var port string
+	if port = os.Getenv("PORT"); len(port) == 0 {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
